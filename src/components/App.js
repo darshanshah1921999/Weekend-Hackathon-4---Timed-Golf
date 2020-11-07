c88289a57329100fa010523dd5df806cb8dfd867
darshanshah1921999 <54939839+darshanshah1921999@users.noreply.github.com>
2020-11-07 18:42:37 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -3,22 +3,78 @@ import "../styles/App.css";
 class Timer extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { time: 0, x: 0, y: 0 };
-  }
-  componentDidMount() {
-    
+    this.state = {
+      time: 0,
+      x: 0,
+      y: 0,
+      isStart: false,
+      position: { top: "0px", left: "0px" }
+    };
+    this.handleStartClick = this.handleStartClick.bind(this);
+    this.handleBall = this.handleBall.bind(this);
   }
 
-  componentWillUnmount() {
-    
+  handleStartClick() {
+    let copy = { ...this.state };
+    copy.isStart = true;
+    this.setState(copy);
+    this.id = setInterval(() => {
+      this.handleTime();
+    }, 1000);
+    document.addEventListener("keydown", this.handleBall);
   }
 
+  handleTime() {
+    let copy = { ...this.state };
+    copy.time = Number(copy.time) + 1;
+    this.setState(copy);
+  }
 
+  handleBall(event) {
+    let copy = { ...this.state };
+    if (event.code === "ArrowUp") {
+      copy.y = Number(copy.y) - 5;
+    } else if (event.code === "ArrowRight") {
+      copy.x = Number(copy.x) + 5;
+    } else if (event.code === "ArrowDown") {
+      copy.y = Number(copy.y) + 5;
+    } else if (event.code === "ArrowLeft") {
+      copy.x = Number(copy.x) - 5;
+    }
+    copy.position = {
+      left: copy.x + "px",
+      top: copy.y + "px"
+    };
+    this.setState(copy);
+    console.log(this.state);
+  }
+
+  componentDidMount() {}
+
+  componentDidUpdate() {
+    if (this.state.x === 250 && this.state.y === 250) {
+      clearInterval(this.id);
+      document.removeEventListener("keydown", this.handleBall);
+    }
+  }
+
+  componentWillUnmount() {}
 
   render() {
     return (
- <>
-</>
+      <>
+        {!this.state.isStart ? (
+          <button className="start" onClick={this.handleStartClick}>
+            Start
+          </button>
+        ) : (
+          <>
+            <div className="ball" style={this.state.position}></div>
+            <div className="hole"></div>
+            <div className="heading-timer">{this.state.time}</div>
+          </>
+        )}
+      </>
     );
   }
 }
